194bdcb2eb2e85a486da9b5ecc9e06715ba64b4f
Ketan-Gehlot <159412152+Ketan-Gehlot@users.noreply.github.com>
2024-10-29 06:50:52 -0700

    circlearea.cpp

---
+++ circlearea.cpp
@@ -0,0 +1,14 @@
+//FIND AREA OF CIRCLE
+#include<iostream>
+using namespace std;
+int main(){
+int radius;
+float pie=3.14,area;
+cout<<"ENTER THE RADIUS OF CIRCLE: ";
+cin>>radius;
+area=2*pie*radius;
+cout<<"The Area of Circle is "<<area;
+return 0;
+}
+
+//area=pie*radius*radius for  area of circle
